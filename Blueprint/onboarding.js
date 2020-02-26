fee84f947c0d04be2d5e70e8d52c1fe7372852ae
Yong Wei <yongwei996@gmail.com>
2020-02-26 12:39:43 +0800

    created the welcome page, and added the search bar not completed yet

---
+++ Blueprint/onboarding.js
@@ -0,0 +1,60 @@
+let showing = 0;
+let slides = $(".dot");
+nextSlide();
+
+function nextSlide() {
+//   if (showing == 2) {
+//     $("#skip").hide();
+//     // $("#next-circle").text("Let's go");
+//   }  
+  let target = slides[showing];
+  $(target)
+  .addClass('fill')
+  .siblings("div")
+  .removeClass("fill");
+  
+  var margin = -100 * showing 
+  $('.slider').css("margin-left", margin + "%");
+}
+
+
+// function goHome() {
+//   console.log('Go home');
+//   chrome.browserAction.setPopup({ popup: "home.html" });
+//   window.location.href = "home.html";
+// }
+
+$("#next-circle").click(function () {
+    if (showing < 1) {
+        showing += 1;
+        //   if (showing >= 3) {
+        //     goHome()
+        //   } else {
+        console.log(showing);
+        nextSlide();
+        //   }
+
+    }
+});
+
+$("#previous-circle").click(function () {
+    if (showing > 0) {
+        showing -= 1;
+        //   if (showing >= 3) {
+        //     goHome()
+        //   } else {
+        console.log(showing);
+        nextSlide();
+        //   }
+    }
+});
+
+
+// $("#skip").click(function() {
+//   goHome()
+// });
+
+$('#close-icon').click(function() {
+  console.log('close');
+  window.close();
+})
--- Blueprint/readme.txt
+++ Blueprint/readme.txt
@@ -1,2 +1,5 @@
 This is the BT garage extension 
 create manifest file
+
+Tutorial for search bar
+https://youtu.be/v1PeTDrw6OY
