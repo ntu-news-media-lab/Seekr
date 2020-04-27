79eee12168b6944d26eae6bb45ffc42ff0b7bef8
Yong Wei <yongwei996@gmail.com>
2020-04-27 11:31:16 +0800

    updated master branch

--- old programs/Python-Chrome-App-Native-Messaging-master/Python-Chrome-App-Native-Messaging-master/app/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {
-    'alwaysOnTop' : true,
-    'outerBounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
-});
